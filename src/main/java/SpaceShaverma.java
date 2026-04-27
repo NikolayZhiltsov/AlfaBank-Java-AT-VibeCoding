dfd7d031756054b1510d6e6ca87f5902e28bfd30
NikolayZhiltsov <n.zhiltsov@yandex.ru>
2026-04-27 17:34:58 +0400

    Добавил задания из первой и второй лекции в репозиторий

---
+++ src/main/java/SpaceShaverma.java
@@ -0,0 +1,14 @@
+public class SpaceShaverma {
+	public static void main(String[] args) {
+		String workerFullName = "Космоторговцев Шаверман Горчицевич";
+		String workingPosition = "Старший помощник младшего торговца шавермой 86-го уровня";
+		int rate = 100500;
+		int numberOfShifts = 22;
+		int salaryBonus = 100500;
+		int penaltyForBurntPitaBread = 2000000;
+		int priceOfOneShaverma = 55000;
+		int numberOfShavermaSold = 675;
+		
+		System.out.println ("Имя сотрудника: " + workerFullName + "\nДолжность: " + workingPosition + "\nОплата за смены: " + numberOfShifts * rate + "\nПремия: " + salaryBonus + "\nШтраф: " + penaltyForBurntPitaBread + "\nИтоговая зарплата: " + (rate * numberOfShifts + salaryBonus - penaltyForBurntPitaBread) + "\nШаур-выручка: " + priceOfOneShaverma * numberOfShavermaSold);
+	}
+}
